e181e869b78be39cee492e3fd66e2ef423cc063b
Harsh Tripathi <ht87374@gmail.com>
2022-06-13 22:00:20 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  let list = [];
+
+  function RGBToHex(r, g, b) {
+    r = r.toString(16);
+    g = g.toString(16);
+    b = b.toString(16);
+
+    if (r.length === 1) r = "0" + r;
+    if (g.length === 1) g = "0" + g;
+    if (b.length === 1) b = "0" + b;
+
+    return "#" + r + g + b;
+  }
+
+  for (let index = 0; index < 63; index++) {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    const hex = RGBToHex(r, g, b);
+    console.log(hex);
+    console.log(r + " " + g + " " + b);
+    list.push(
+      <div
+        className="divs"
+        style={{ backgroundColor: `${hex}` }}
+      >
+        <span className="hex-text">{hex}</span>
+      </div>
+    );
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <header className="header">
+		<h2>Colorify</h2>
+		<h2 className="refresh" onClick={(e) => {
+			window.location.reload(false);
+		}}>Refresh</h2>
+	  </header>
+      <div className="main">{list}</div>
+	  <footer>
+		<p>	&#169; 2022 Colorify - Made with ❤️</p>
+	  </footer>
+    </>
   );
 }
 
